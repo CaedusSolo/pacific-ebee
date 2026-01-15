de141b20c582e62f3a06650bc801af0b63b21007
Ying Tong <yingtong0106@gmail.com>
2026-01-15 14:54:49 +0800

    feat: ServerConnection class

--- src/server.cpp
+++ src/server.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "Connection.h"
 
 using namespace std;
 
-class ServerConnection {
+class ServerConnection : public Connection {
 private:
-    int port = 8000;
-    int serverFD;
+    int port;
 
 public:
-    ServerConnection(int portNum) : port(portNum) {}
-
-    void startConnection() {
-        cout << "Server started on port " << port << endl;
+    ServerConnection(int portNum) 
+        : Connection(socket(AF_INET, SOCK_STREAM, 0)), port(portNum) {
+        if (getFD() < 0) {
+            perror("Socket creation failed");
+            exit(EXIT_FAILURE);
+        }
     }
 
-};
+    void startConnection() {
+        struct sockaddr_in address;
+        int opt = 1;
 
-int main (int argc, char *argv[]) {
-    return 0;
-}
+        if (setsockopt(getFD(), SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(port); // Host to Network Short
+
+        if (bind(getFD(), (struct sockaddr *)&address, sizeof(address)) < 0) {
+            perror("Bind failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(getFD(), 2) < 0) {
+            perror("Listen failed");
+            exit(EXIT_FAILURE);
+        }
+
+        cout << "Server listening on port " << port << "..." << endl;
+    }
+    // create individual player connections
+    Connection* acceptPlayer() {
+        struct sockaddr_in clientAddr;
+        int addrLen = sizeof(clientAddr);
+        cout << "Waiting for connection..." << endl;
+
+        int newFD = accept(getFD(), (struct sockaddr*)&clientAddr, (socklen_t*)&addrLen);
+
+        if (newFD < 0) {
+            perror("Accept failed");
+            return nullptr;
+        }
+
+        // return a new wrapper for client's socket
+        return new Connection(newFD); 
+    }
+};
