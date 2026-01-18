a160914c43c0c2a59157c78096ad2dc3570a3bb8
Mimi <aimimf20@gmail.com>
2026-01-19 00:30:34 +0800

    chore: fixed constant header name lol

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,7 @@
-#ifndef CONSTATNTS_H
-#define CONSTATNTS_H
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 
 const int BATTLEFIELD_SIZE = 25;  // considering max players is 5
 const int PLAYER_NUM = 3;  //temporary placeholder
 
-#endif  // CONSTATNTS_H
+#endif  // CONSTANTS_H
