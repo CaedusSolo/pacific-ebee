b0853ee292ec87935752d95974cc737ab7722061
Mimi <aimimf20@gmail.com>
2026-01-20 18:15:04 +0800

    feat: added bg colours for attacked area

--- src/renderer.h
+++ src/renderer.h
@@ -9,12 +9,17 @@
 using namespace std;
 
 namespace ANSI {  
+    
     static constexpr const char* BLUE = "\033[34m";  
-    static constexpr const char* RESET = "\033[0m";  
     static constexpr const char* ORANGE = "\033[33m";  
     static constexpr const char* RED = "\033[31m";  
     static constexpr const char* GREEN = "\033[32m";
     static constexpr const char* PINK = "\033[95m";
+
+    static constexpr const char* BG_BLUE = "\033[44m";
+    static constexpr const char* BG_GREY = "\033[100m";
+    
+    static constexpr const char* RESET = "\033[0m";  
     static constexpr const char* SLOW_BLINK = "\033[5m";  
     static constexpr const char* RAPID_BLINK = "\033[6m";  
     static constexpr const char* STOP_BLINK = "\033[25m";  
