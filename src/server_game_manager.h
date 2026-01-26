539786d7dbc8ab809ba21d1dfb082184663410e4
k3mystra <programmer.coleslaw981@slmail.me>
2026-01-26 12:43:41 +0800

    chore: Add missing vector2D and vector

--- src/server_game_manager.h
+++ src/server_game_manager.h
@@ -1,14 +1,17 @@
 #ifndef SERVER_GAME_MANAGER_H
 #define SERVER_GAME_MANAGER_H
-#include "vector"
+
+#include "vector2d.h"
 #include "renderer.h"
+
+#include <vector>
 
 using namespace std;
 
 struct Shot{
     Vector2D coord;
     bool isHit;
-}
+};
 
 class ServerGameManager{
 private:
