9af7e9a2ac748e5783000b05f572014f238180b6
choongjax@gmail.com <choongjax@gmail.com>
2026-01-31 14:12:48 +0800

    feat: added cell class

---
+++ src/Cell.h
@@ -0,0 +1,22 @@
+#ifdef  CELL_H
+#define CELL_H
+
+#include "vector2d.h"
+#include "vector"
+
+using namespace std;
+
+class Cell{
+    private:
+        Vector2D coordinates;
+        vector<Ship> ships;    // ships in this cell
+    public:
+        Cell();
+        Cell(Vector2D coordinates);
+        Vector2D getCoordinates() const;
+        vector<Ship> getShips() const;
+        void addShip(Ship ship);
+        void removeShip(Ship ship);
+};
+
+#endif
