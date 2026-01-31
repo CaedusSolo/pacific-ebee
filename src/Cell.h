86b9c2a9225d48d565e253c168ee9a6cca03436c
choongjax@gmail.com <choongjax@gmail.com>
2026-01-31 14:15:14 +0800

    feat: added one more isEmpty for cell

--- src/Cell.h
+++ src/Cell.h
@@ -13,6 +13,7 @@ class Cell{
     public:
         Cell();
         Cell(Vector2D coordinates);
+        bool isEmpty() const;
         Vector2D getCoordinates() const;
         vector<Ship> getShips() const;
         void addShip(Ship ship);
