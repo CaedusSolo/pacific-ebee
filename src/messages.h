fa879c5c997e8e0d647522373b8274e14e82e9be
k3mystra <programmer.coleslaw981@slmail.me>
2026-02-05 01:11:50 +0800

    feat: Message.h to C

--- src/messages.h
+++ src/messages.h
@@ -3,9 +3,8 @@
 
 #include "vector2d.h"
 #include "game_states.h"
-#include "serializable.h"
 
-struct HitResult : public Serializable<HitResult> {
+struct HitResult {
     enum class Type {
         MISS,
         HIT,
@@ -13,10 +12,6 @@ struct HitResult : public Serializable<HitResult> {
     } type;
     Vector2D position;
     Ship shipSunk; // Valid only if type is SINK
-
-    // Serialization implementation
-    char* toBytesImpl() const;
-    static HitResult fromBytesImpl(const char* data);
 };
 
 #endif // !MESSAGES_H
