f7d1b362cd9d6fc072c61548bfdd3b31a32d104c
k3mystra <programmer.coleslaw981@slmail.me>
2026-02-07 23:59:45 +0800

    chore: convetiosnfja

--- src/battlefield.h
+++ src/battlefield.h
@@ -9,12 +9,12 @@ typedef struct {
     int height;
 } Battlefield;
 
-Battlefield* Battlefield_new(int width, int height);
-void Battlefield_delete(Battlefield* self);
-Cell* Battlefield_getCell(const Battlefield* self, int x, int y);
-int Battlefield_getWidth(const Battlefield* self);
-int Battlefield_getHeight(const Battlefield* self);
-void Battlefield_updateCell(Battlefield* self, int x, int y, Cell cell);
-void Battlefield_placeShipsRandomly(Battlefield* self, int player_index);
+Battlefield* battlefield_new(int width, int height);
+void battlefield_delete(Battlefield* self);
+Cell* battlefield_get_cell(const Battlefield* self, int x, int y);
+int battlefield_get_width(const Battlefield* self);
+int battlefield_get_height(const Battlefield* self);
+void battlefield_update_cell(Battlefield* self, int x, int y, Cell cell);
+void battlefield_place_ships_randomly(Battlefield* self, int player_index);
 
 #endif
--- src/cell.c
+++ src/cell.c
@@ -1,7 +1,7 @@
 #include "cell.h"
 #include <stdlib.h>
 
-Cell* Cell_new() {
+Cell* cell_new() {
     Cell* self = (Cell*)malloc(sizeof(Cell));
     self->coordinates = vector2d_create(0, 0);
     for (int i = 0; i < PLAYER_NUM; i++) {
@@ -11,7 +11,7 @@ Cell* Cell_new() {
     return self;
 }
 
-Cell* Cell_new_with_coordinates(Vector2D coordinates) {
+Cell* cell_new_with_coordinates(Vector2D coordinates) {
     Cell* self = (Cell*)malloc(sizeof(Cell));
     self->coordinates = coordinates;
     for (int i = 0; i < PLAYER_NUM; i++) {
@@ -21,17 +21,17 @@ Cell* Cell_new_with_coordinates(Vector2D coordinates) {
     return self;
 }
 
-void Cell_delete(Cell* self) {
+void cell_delete(Cell* self) {
     if (self) {
         free(self);
     }
 }
 
-Vector2D Cell_getCoordinates(const Cell* self) {
+Vector2D cell_get_coordinates(const Cell* self) {
     return self->coordinates;
 }
 
-bool Cell_isEmpty(const Cell* self) {
+bool cell_is_empty(const Cell* self) {
     // Check if completely empty (no ships from any player)
     for (int i = 0; i < PLAYER_NUM; i++) {
         if (self->has_ship[i]) {
@@ -41,17 +41,17 @@ bool Cell_isEmpty(const Cell* self) {
     return true;
 }
 
-bool Cell_hasPlayerShip(const Cell* self, int player_index) {
+bool cell_has_player_ship(const Cell* self, int player_index) {
     if (player_index >= 0 && player_index < PLAYER_NUM) {
         return self->has_ship[player_index];
     }
     return false;
 }
 
-void Cell_addShip(Cell* self, void* ship) {
+void cell_add_ship(Cell* self, void* ship) {
     // TODO: Implement ship array/list
 }
 
-void Cell_removeShip(Cell* self, void* ship) {
+void cell_remove_ship(Cell* self, void* ship) {
     // TODO: Implement ship removal
 }
