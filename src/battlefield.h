d4058e0361097763062d559d37218edce80dfb1d
k3mystra <programmer.coleslaw981@slmail.me>
2026-02-08 02:17:18 +0800

    feat: Add serialization to Battlfield

--- src/battlefield.h
+++ src/battlefield.h
@@ -4,17 +4,19 @@
 #include "cell.h"
 
 typedef struct {
-    Cell** grid;  // 2D array of Cells
     int width;
     int height;
+    Cell** grid;  // 2D array of Cells
 } Battlefield;
 
-Battlefield* battlefield_new(int width, int height);
+Battlefield battlefield_new(int width, int height);
 void battlefield_delete(Battlefield* self);
 Cell* battlefield_get_cell(const Battlefield* self, int x, int y);
 int battlefield_get_width(const Battlefield* self);
 int battlefield_get_height(const Battlefield* self);
 void battlefield_update_cell(Battlefield* self, int x, int y, Cell cell);
 void battlefield_place_ships_randomly(Battlefield* self, int player_index);
+int battlefield_serialize(const Battlefield* bf, char* buffer);
+int battlefield_deserialize(Battlefield* bf, const char* buffer);
 
 #endif
